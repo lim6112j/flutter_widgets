984ff0847adcb7d270f0cbceed4df0f86fd7b71d
%NAME% <%EMAIL%>
2025-11-08 11:37:40 +0900

    animated menu

--- apps/flutter_animate_app/lib/components/menu.dart
+++ apps/flutter_animate_app/lib/components/menu.dart
@@ -7,7 +7,21 @@ class Menu extends StatefulWidget {
   State<Menu> createState() => _MenuState();
 }
 
-class _MenuState extends State<Menu>{
+class _MenuState extends State<Menu> with SingleTickerProviderStateMixin{
+  final List<Interval> _itemSlideIntervals = [];
+  late Interval _buttonInterval;
+  static const _initialDelayTime = Duration(milliseconds: 50);
+  static const _itemSlideTime = Duration(milliseconds: 250);
+  static const _staggerTime = Duration(milliseconds: 50);
+  static const _buttonDelayTime = Duration(milliseconds: 150);
+  static const _buttonTime = Duration(milliseconds: 500);
+  final _animationDuration =
+    _initialDelayTime +
+  (_staggerTime * _menuTitles.length) +
+  _buttonDelayTime +
+  _buttonTime;
+
+  late AnimationController _staggeredController;
   static const _menuTitles = [
     'Declarative Style',
     'Premade Widgets',
@@ -15,7 +29,38 @@ class _MenuState extends State<Menu>{
     'Native Performance',
     'Great Community',
   ];
-
+  @override
+  void initState() {
+    super.initState();
+    _createAnimationIntervals();
+    _staggeredController = AnimationController(
+      duration: _animationDuration,
+      vsync: this,
+    )..forward();
+  }
+  void _createAnimationIntervals() {
+        for (var i = 0; i < _menuTitles.length; ++i) {
+      final startTime = _initialDelayTime + (_staggerTime * i);
+      final endTime = startTime + _itemSlideTime;
+      _itemSlideIntervals.add(
+        Interval(
+          startTime.inMilliseconds / _animationDuration.inMilliseconds,
+          endTime.inMilliseconds / _animationDuration.inMilliseconds,
+        ),
+      );
+      final buttonStartTime = Duration(milliseconds: _menuTitles.length * 50) + _buttonDelayTime;
+      final buttonEndTime = buttonStartTime + _buttonTime;
+      _buttonInterval = Interval(
+        buttonStartTime.inMilliseconds / _animationDuration.inMilliseconds,
+        buttonEndTime.inMilliseconds / _animationDuration.inMilliseconds,
+      );
+    }
+  }
+  @override
+  void dispose() {
+    _staggeredController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -43,17 +88,34 @@ class _MenuState extends State<Menu>{
       ],
     );
   }
-
   List<Widget> _buildListItems() {
     final listItems = <Widget>[];
     for (var i = 0; i < _menuTitles.length; ++i) {
       listItems.add(
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 16),
-          child: Text(
-            _menuTitles[i],
-            textAlign: TextAlign.left,
-            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+        AnimatedBuilder(
+          animation: _staggeredController,
+          builder: (context, child) {
+            final animationPercent = Curves.easeOut.transform(
+              _itemSlideIntervals[i].transform(_staggeredController.value),
+            );
+            final opacity = animationPercent;
+            final slideDistance = (1.0 - animationPercent) * 150;
+
+            return Opacity(
+              opacity: opacity,
+              child: Transform.translate(
+                offset: Offset(slideDistance, 0),
+                child: child,
+              ),
+            );
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 16),
+            child: Text(
+              _menuTitles[i],
+              textAlign: TextAlign.left,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+            ),
           ),
         ),
       );
@@ -66,16 +128,31 @@ class _MenuState extends State<Menu>{
       width: double.infinity,
       child: Padding(
         padding: const EdgeInsets.all(24),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            shape: const StadiumBorder(),
-            backgroundColor: Colors.blue,
-            padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 14),
-          ),
-          onPressed: () {},
-          child: const Text(
-            'Get Started',
-            style: TextStyle(color: Colors.white, fontSize: 22),
+        child: AnimatedBuilder(
+          animation: _staggeredController,
+          builder: (context, child) {
+            final animationPercent = Curves.elasticOut.transform(
+              _buttonInterval.transform(_staggeredController.value),
+            );
+            final opacity = animationPercent.clamp(0.0, 1.0);
+            final scale = (animationPercent * 0.5) + 0.5;
+
+            return Opacity(
+              opacity: opacity,
+              child: Transform.scale(scale: scale, child: child),
+            );
+          },
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: const StadiumBorder(),
+              backgroundColor: Colors.blue,
+              padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 14),
+            ),
+            onPressed: () {},
+            child: const Text(
+              'Get Started',
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
           ),
         ),
       ),
