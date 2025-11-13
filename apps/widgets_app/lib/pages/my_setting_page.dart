1b5be4668862453d6bee5e564cc55a986179af3e
%NAME% <%EMAIL%>
2025-11-13 11:18:41 +0900

    setting page

---
+++ apps/widgets_app/lib/pages/my_setting_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class MySettingPage extends StatelessWidget {
+  const MySettingPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Setting Page'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            context.pop();
+          },
+        ),
+      ),
+      body: const Center(
+        child: Text('Setting Page'),
+      ),
+    );
+  }
+}
--- packages/my_dart_package/lib/routes/router.dart
+++ packages/my_dart_package/lib/routes/router.dart
@@ -1,28 +1,14 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 import 'package:widgets_app/pages/my_home_page.dart';
+import 'package:widgets_app/pages/my_setting_page.dart';
 
 final routes = GoRouter(
   routes: [
     GoRoute(
       path: '/search',
       name: 'search',
-      builder: (context, state) => Scaffold(
-        appBar: AppBar(
-          title: Text('Search Page'),
-          leading: IconButton(
-            icon:  Icon(Icons.arrow_back),
-            onPressed: () {
-              // Handle back button press
-              context.pop();
-            },
-          ),
-        ),
-        body: Center(
-          child: Text('search Page'),
-        ),
-      ),
-    ),
+      builder: (context, state) => const MySettingPage()),
     GoRoute(
       path: '/',
       name: 'widgets',
